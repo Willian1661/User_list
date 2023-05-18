0f24619a24bf1bb8c66ac233a0bc2b71ec67cfb2
willian1661 <willianderson.sn@gmail.com>
2023-05-18 19:56:15 -0300

    - created test files - organized directorys - updated eslint and webpack config - added postcss and tailwind config - added new packages to support tailwind

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -2,19 +2,40 @@ module.exports = {
 	"env": {
 		"browser": true,
 		"es2022": true,
+		"node": true
 	},
 	"overrides": [],
+	"root": true,
 	"parser": "@typescript-eslint/parser",
 	"parserOptions": {
 		"ecmaVersion": "latest",
-		"sourceType": "module"
 	},
+
 	"plugins": ["@typescript-eslint"],
-	"extends": ["plugin:@typescript-eslint/recommended"],
+
+	"extends": [
+		"eslint:recommended",
+		"plugin:@typescript-eslint/recommended",
+		"plugin:@typescript-eslint/eslint-recommended",
+	],
 	"rules": {
 		"@typescript-eslint/no-unused-vars": "warn",
-
-		"@typescript-eslint/consistent-type-definitions": "error", 
+		"@typescript-eslint/consistent-type-definitions": "error",
 		"@typescript-eslint/semi": "error",
-        }
+		"@typescript-eslint/no-var-requires": "off",
+		"no-console": "error",
+		"curly": ["error"],
+		"no-empty-pattern": "error",
+		"no-empty-character-class": "error",
+		"no-func-assign": "error",
+		"no-useless-call": "error",
+		"no-useless-catch": "error",
+		"no-lonely-if": "error",
+		"no-duplicate-imports": "error",
+		"no-else-return": "error",
+		"semi": "error",
+		"semi-spacing": "error",
+		"semi-style": ["error","last"],
+		"linebreak-style": ["error","unix"],
+	}
 };
---
+++ client/src/components/_tests_/sum.test.tsx
@@ -0,0 +1,7 @@
+import sum from "../sum";
+
+describe('first test',()=>{
+it('should return a sum', () => {
+    expect(sum(1,1)).toBe(2);
+});
+});
