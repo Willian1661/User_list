523af17ebe0a6d2aaaad18c1bc18dc1d7feedfd2
willianderson <106893704+Willian1661@users.noreply.github.com>
2023-05-20 19:38:57 -0300

    adjust setup (#1)
    * adjusted setup
    
    * - created test files
    - organized directorys
    - updated eslint and webpack config
    - added postcss and tailwind config
    - added new packages to support tailwind

---
+++ client/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const isProduction = process.env.NODE_ENV === 'production';
+
+const config = {
+    entry: {
+        bundle: './src/components/main.tsx'
+    },
+    devtool: 'inline-source-map',
+
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: '[name].js',
+        clean: true,
+    },
+    devServer: {
+        hot: true,
+        static: "./dist",
+    },
+
+    target: 'web',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: ["style-loader", "css-loader", "postcss-loader"],
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+                type: 'asset',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/pages/index.html',
+        }),
+    ],
+};
+
+module.exports = () => {
+    if (isProduction) {
+        config.mode = 'production';
+    } else {
+        config.mode = 'development';
+    }
+    return config;
+};
