45d12e0871fa5f3d9bffed5855b99dca71bcdfa0
willian1661 <willianderson.sn@gmail.com>
2023-07-07 23:31:43 -0300

    - deleted old components - created new component Table - rewrited main to index - modified index.html - modified webpack

--- client/src/components/main.tsx
+++
@@ -1,9 +0,0 @@
-import '../styles/custom.css';
-import '../lib/api';
-  const element = document.createElement('div');
-
-  element.innerHTML = "Hello webpack";
-
-  
-  document.body.appendChild(element);
-
